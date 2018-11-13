1b48a8f3acc39211116d469d2efcbfcba8f4add4
Gutu Galuppo <galuppodev@gmail.com>
2018-11-13 15:21:31 +0100

    Fisrt step page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,36 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function () {
+  document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('TEST JS imported successfully!');
+    console.log('TEST JS imported successfully!');
 
-}, false);
+  }, false);
+
+  $("#sport").click(function () {
+    $("#toggle1").toggle();
+  });
+
+  $("#music").click(function () {
+    $("#toggle2").toggle();
+  });
+
+  $("#lrngrp").click(function () {
+    $("#toggle3").toggle();
+  });
+
+  $("#lgprf").click(function () {
+    $("#toggle4").toggle();
+  });
+
+  $("#culinary").click(function () {
+    $("#toggle5").toggle();
+  });
+
+  $("#location").click(function () {
+    $("#toggle6").toggle();
+  });
+  
+
+  $("#getintch").click(function () {
+    $("#toggle7").toggle();
+  });
+});
